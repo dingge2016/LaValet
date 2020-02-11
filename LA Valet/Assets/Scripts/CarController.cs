3f92ba062fbe099c14e0a8dff81c421823a8c89e
dingge2016 <dingy8@foxmail.com>
2020-02-11 15:39:56 -0800

    grid move

--- LA Valet/Assets/Scripts/CarController.cs
+++ LA Valet/Assets/Scripts/CarController.cs
@@ -1,48 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
 
 public class CarController : MonoBehaviour
 {
-         
-         Vector3 dist;
-         Vector3 startPos;
-         float posX;
-         float posY;
-         float posZ;
-         
-         void OnMouseDown()
-         {
-             startPos = transform.position;
-             dist = Camera.main.WorldToScreenPoint(transform.position);
-             posX = Input.mousePosition.x - dist.x;
-             posY = Input.mousePosition.y - dist.y;
-             posZ = Input.mousePosition.z - dist.z;
-         }
- 
-         void OnMouseDrag()
-         {
-             float disX = Input.mousePosition.x - posX;
-             float disY = Input.mousePosition.y - posY;
-             float disZ = Input.mousePosition.z - posZ;
-             Vector3 lastPos = Camera.main.ScreenToWorldPoint(new Vector3(disX, disY, disZ));
-             transform.position = new Vector3(lastPos.x, lastPos.y, lastPos.z);
-         }
-         
-         /*
-         
-         private Vector3 screenPoint;
 
-         void OnMouseDown(){
-            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-         }
+    Vector3 dist;
+    Vector3 startPos;
+    float posX;
+    float posY;
+    float posZ;
 
-         void OnMouseDrag(){
-            Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-            Vector3 currentPos = Camera.main.ScreenToWorldPoint(currentScreenPoint);
-            transform.position = currentPos;
-         }
-         */
-         
+    void OnMouseDown()
+    {
+        startPos = transform.position;
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+        posZ = Input.mousePosition.z - dist.z;
+    }
+
+    void OnMouseDrag()
+    {
+        float disX = Input.mousePosition.x - posX;
+        float disY = Input.mousePosition.y - posY;
+        float disZ = Input.mousePosition.z - posZ; 
+        Vector3 lastPos = Camera.main.ScreenToWorldPoint(new Vector3(disX, disY, disZ));
+
+        // Debug.Log("X:" + lastPos.x + "Y:" + lastPos.y + " Z:" + lastPos.z);
+        transform.position = new Vector3(lastPos.x, 0.7f, lastPos.z);
+    }
+
+  
+
 
 }
