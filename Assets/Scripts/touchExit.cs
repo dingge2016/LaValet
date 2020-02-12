608140fa0d0df191d07da2f563cf25b7c9266055
dingge2016 <dingyue@usc.edu>
2020-02-11 16:57:12 -0800

    Add Exit Feature(beta 1)
    1. change the parking lot
    2. add exit wall
    3. Fixed bug (remove rigidbody)

---
+++ Assets/Scripts/touchExit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchExit : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("A car"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
