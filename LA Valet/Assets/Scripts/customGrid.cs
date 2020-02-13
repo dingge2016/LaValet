1b5a3788ad6db4ad92a99ddc0dffe8377b751b85
XiaotongJiang <32645163+CallMeMajorTom@users.noreply.github.com>
2020-02-12 23:38:31 -0800

    Revert "Collision With Walls"
    This reverts commit 858c6b7718cb2d8faa2886756260cdd1d81d6cbb.

--- LA Valet/Assets/Scripts/customGrid.cs
+++ LA Valet/Assets/Scripts/customGrid.cs
@@ -10,24 +10,24 @@ public class customGrid : MonoBehaviour
     public float gridSize;
     private Collider collider;
     Vector3 truePos;
-
+     
     void LateUpdate()
     {
         truePos.x = Mathf.Round(target.transform.position.x / gridSize) * gridSize;
      //   truePos.y = Mathf.Round(target.transform.position.y / gridSize) * gridSize;
         truePos.z = Mathf.Round(target.transform.position.z / gridSize) * gridSize;
-
+        
         if (structure.transform.position.z != truePos.z && Mathf.Round(structure.transform.rotation.y) == 0 )
         {
+            
 
-
-            structure.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
+            structure.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self); 
 
         }
         if (structure.transform.position.x != truePos.x && Mathf.Round(structure.transform.rotation.y) != 0)
         {
-            //Debug.Log("hereeeeeee "+ structure.transform.rotation.y);
-
+            Debug.Log("hereeeeeee "+ structure.transform.rotation.y);
+           
             //  && Mathf.Round(structure.transform.rotation.y) == 90.0f;
           /*  Debug.Log("OriginalPos: " + structure.transform.position.x + " " + structure.transform.position.z);
             Debug.Log("truePos: " + truePos.x + " " + truePos.z); */
